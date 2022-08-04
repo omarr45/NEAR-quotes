28b31d9a8f6e0d0f8a5a0ca2ca5b7ba26b6adaec
omarr45 <58887202+omarr45@users.noreply.github.com>
2022-08-04 16:29:22 +0200

    minor updates

--- assembly/index.ts
+++ assembly/index.ts
@@ -4,13 +4,13 @@ import { Quote } from './model';
 
 @nearBindgen
 export class Contract {
-  public quotes: PersistentVector<Quote> = new PersistentVector<Quote>('q');
+  public quotes: PersistentVector<Quote> = new PersistentVector<Quote>('qs');
 
   @mutateState()
   addQuote(quote: string): Quote {
-    let writer: string = Context.sender;
+    const writer: string = Context.sender;
 
-    let q = new Quote(quote, writer);
+    const q = new Quote(quote, writer);
 
     this.quotes.push(q);
     return q;
